7b0b92e3bdae7f3a449da116a9ca2ff72c2d7914
Renan Rezende <renan3j1@hotmail.com>
2023-06-10 22:00:54 -0300

    Commit 2.0 every ok

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 
-
-# from recipes import views
-# (o . indica que o arquivo que está sendo buscado é irmão deste arquivo, ou seja, da pasta que estamos importe views)
 from . import views
 
-# recipes:recipe
 app_name = 'recipes'
 
-# dominio/recipes/...
 urlpatterns = [
-    path('', views.home, name="home"),  # HOME
-    path('recipes/<int:id>/', views.recipes, name="recipe"),
-
+    path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
